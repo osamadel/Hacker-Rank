9c50e4c036b39b52e5d4dcdd0d49c61c83ef8738
Osama Adel <o.adel@devisionx.com>
2018-11-27 21:04:37 +0200

    60% solution to climbing the leaderboard problem

--- Implementation/climbing_the_leaderboard.py
+++ Implementation/climbing_the_leaderboard.py
@@ -8,17 +8,15 @@ import os
 import random
 import re
 import sys
+import time
 
 
 def build_leaderboard(scores):
-    # current_score and last_score
     last_score = scores[0]
-    # current_rank and last_rank
     rank = 1
     leaderboard = [[rank, last_score]]
     for score in scores[1:]:
         if score != last_score:
-            # if the current score and last score are not equal, then both have the same ranking.
             rank += 1
             leaderboard.append([rank, score])
         else:
@@ -27,25 +25,27 @@ def build_leaderboard(scores):
     return leaderboard
 
 
-def insert_score(leaderboard, score, index):
-    rank = leaderboard[index][0]
-    leaderboard.insert(index, [rank, score])
-    return rank
-
-
-def find_rank(alice_score, leaderboard):        # How to traverse a list in reverse?!
-    for i, score in enumerate(leaderboard):
-        if alice_score >= score[1]:
-            return i, insert_score(leaderboard, alice_score, i)
-    return len(leaderboard) - 1, leaderboard[-1][0] + 1
+def find_rank(alice_score, leaderboard):
+    leaderboard_length = len(leaderboard)
+    for i in range(leaderboard_length):
+        i_inv = leaderboard_length - 1 - i
+        if alice_score < leaderboard[i_inv][1]:
+            return i_inv, leaderboard[i_inv][0] + 1
+        elif alice_score == leaderboard[i_inv][1]:
+            return i_inv, leaderboard[i_inv][0]
+    return 0, 1
 
 
 # Complete the climbingLeaderboard function below.
 def climbingLeaderboard(scores, alice):
+    print('======================== LEADERBOARD ========================')
+    print('scores=', len(scores), '\nalice=', len(alice))
+    time.sleep(5)
     alice_ranks = []
     leaderboard = build_leaderboard(scores)
     for alice_score in alice:
         last_index, rank = find_rank(alice_score, leaderboard)
+        print('rank=', rank)
         alice_ranks.append(rank)
         leaderboard = leaderboard[:last_index + 1]
     return alice_ranks
