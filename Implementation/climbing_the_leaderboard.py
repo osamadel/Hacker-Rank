4b5b1c5d1e37f10a02313c4cf6856c8e8988a160
Osama Adel <o.adel@devisionx.com>
2018-12-01 00:04:32 +0200

    unsuccessful solution to climing the leaderboard problem.

--- Implementation/climbing_the_leaderboard.py
+++ Implementation/climbing_the_leaderboard.py
@@ -10,6 +10,41 @@ import re
 import sys
 import time
 
+def build_leaderboard_alt(scores):
+    last_s = scores[0]
+    leaderboard = [last_s]
+    for s in scores[1:]:
+        if s != last_s:
+            leaderboard.append(s)
+        last_s = s
+    return leaderboard
+
+
+def find_rank_alt(alice_score, leaderboard):
+    l = leaderboard[:]
+    i = len(l) // 2
+    idx = i
+    while True:
+        if alice_score < l[i]:
+            l = l[i:]
+            i = len(l) // 2
+            idx += i
+        elif alice_score > l[i]:
+            l = l[:i]
+            i = len(l) // 2
+            # idx -= (1 + i) if i%2 == 0 else i
+            idx -= (len(l) - i)
+        else:
+            l = [l[i]]
+            idx = i
+        print('i=', i, '\tidx=', idx)
+        if len(l) == 1:
+            # idx+1 < len(leaderboard) and 
+            if alice_score < leaderboard[idx]: idx += 1
+            break
+        elif len(l) == 0: break
+    return idx
+
 
 def build_leaderboard(scores):
     last_score = scores[0]
@@ -38,18 +73,33 @@ def find_rank(alice_score, leaderboard):
 
 # Complete the climbingLeaderboard function below.
 def climbingLeaderboard(scores, alice):
-    print('======================== LEADERBOARD ========================')
-    print('scores=', len(scores), '\nalice=', len(alice))
-    time.sleep(5)
+    # print('======================== LEADERBOARD ========================')
+    # print('scores=', len(scores), '\nalice=', len(alice))
+    # time.sleep(5)
     alice_ranks = []
     leaderboard = build_leaderboard(scores)
     for alice_score in alice:
         last_index, rank = find_rank(alice_score, leaderboard)
-        print('rank=', rank)
+        # print('rank=', rank)
         alice_ranks.append(rank)
         leaderboard = leaderboard[:last_index + 1]
     return alice_ranks
 
+
+def climbingLeaderboard_alt(scores, alice):
+    # print('======================== LEADERBOARD ========================')
+    # print('scores=', len(scores), '\nalice=', len(alice))
+    # time.sleep(5)
+    alice_ranks = []
+    leaderboard = build_leaderboard_alt(scores)
+    
+    for alice_score in alice:
+        # print('leaderobard:', leaderboard)
+        index = find_rank_alt(alice_score, leaderboard)
+        # print('rank=', index+1)
+        alice_ranks.append(index+1)
+        leaderboard = leaderboard[:index+1]
+    return alice_ranks
 
 if __name__ == '__main__':
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -58,7 +108,7 @@ if __name__ == '__main__':
     scores = list(map(int, input().rstrip().split()))
     alice_count = int(input())
     alice = list(map(int, input().rstrip().split()))
-    result = climbingLeaderboard(scores, alice)
+    result = climbingLeaderboard_alt(scores, alice)
     print('\n'.join(map(str,result)))
 
     # fptr.write('\n'.join(map(str, result)))
